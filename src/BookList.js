e8fbd17c2d5a1820be55055d87bc619c88c7e971
yushan <yushan@intervest.lk>
2021-01-30 20:53:59 +0530

    feat: Add a single prop to pass the book data into Book component

--- src/BookList.js
+++ src/BookList.js
@@ -4,9 +4,9 @@ const BookList = props => {
     return (
         <div className="bookshelf-books">
             <ol className="books-grid">
-                props.books.map(book => (
-                    <Book key={book.id} image={book.smallThumbnail} title={book.title} authors={book.authors} shelves={shelves} />
-                ))
+                {props.books.map(book => (
+                    <Book key={book.id} book={book} shelves={shelves} />
+                ))}
             </ol>
         </div>
     )
