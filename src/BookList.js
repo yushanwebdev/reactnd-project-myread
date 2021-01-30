ad09190ab7ba3b6b74483793cc8b02bfc869c10e
yushan <yushan@intervest.lk>
2021-01-30 20:51:07 +0530

    feat: Add shelves prop to the BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -5,7 +5,7 @@ const BookList = props => {
         <div className="bookshelf-books">
             <ol className="books-grid">
                 props.books.map(book => (
-                    <Book key={book.id} image={book.smallThumbnail} title={book.title} authors={book.authors} />
+                    <Book key={book.id} image={book.smallThumbnail} title={book.title} authors={book.authors} shelves={shelves} />
                 ))
             </ol>
         </div>
