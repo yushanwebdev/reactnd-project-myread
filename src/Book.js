81ab7fcd24a0f9764212e85db844f2c753fc1293
yushan <yushan@intervest.lk>
2021-01-30 20:42:11 +0530

    feat: Develop the Book component

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,20 @@
-import ShelfDropDown from "./ShelfDropDown"
+import ShelfSelect from "./ShelfSelect"
 
 const Book = props => {
+    const { image, shelves, title, authors } = props;
     return (
         <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <ShelfDropDown />
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
+                    <ShelfSelect shelves={shelves} />
                 </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{
+                    authors.map(author => (
+                        <p key={author}>{author}</p>
+                    ))
+                }</div>
             </div>
         </li>
     )
