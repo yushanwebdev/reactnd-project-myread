5d954123001dea1b77f9fc9cf8204e01b6a9d28e
yushan <yushan@intervest.lk>
2021-01-30 21:05:09 +0530

    feat: Develop the Shelf component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,10 +1,11 @@
-import BookList from "./BookList"
+import BookList from "./BookList";
 
 const BookShelf = props => {
+    const { title, books, shelves } = props;
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <BookList />
+            <h2 className="bookshelf-title">{title}</h2>
+            <BookList books={books} shelves={shelves} />
         </div>
     )
 }
