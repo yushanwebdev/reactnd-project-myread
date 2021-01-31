1a06c324203ef31899afcb4395f96447b129f4b3
yushan <yushan@intervest.lk>
2021-01-31 17:11:04 +0530

    style: Format the props of ShelfSelect component

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,11 @@ const Book = props => {
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${smallThumbnail})` }}></div>
-                    <ShelfSelect shelves={shelves} book={book} updateBookShelf={updateBookShelf} />
+                    <ShelfSelect 
+                        shelves={shelves} 
+                        book={book} 
+                        updateBookShelf={updateBookShelf} 
+                    />
                 </div>
                 <div className="book-title">{title}</div>
                 <div className="book-authors">{
