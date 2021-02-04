01066a05b04cf7677393509422228e4adbdea794
yushan <yushan@intervest.lk>
2021-02-04 14:06:23 +0530

    fix: Fix error throw when imageLinks undefined

--- src/Book.js
+++ src/Book.js
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 import ShelfSelect from "./ShelfSelect";
 
 const Book = props => {
-    const { book, book: { imageLinks: { smallThumbnail }, title, authors }, shelves, updateBookShelf } = props;
+    const { book, book: { imageLinks = "https://dl.acm.org/specs/products/acm/releasedAssets/images/cover-default--book.svg", title, authors }, shelves, updateBookShelf } = props;
     return (
         <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${smallThumbnail})` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.smallThumbnail})` }}></div>
                     <ShelfSelect 
                         shelves={shelves} 
                         book={book} 
